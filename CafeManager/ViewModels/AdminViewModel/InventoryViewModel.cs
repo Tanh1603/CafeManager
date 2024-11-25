b49e40fd83cdb7ddc42650c5a47bc556e78538e9
LeVanHuy84 <huyle842005@gmail.com>
2024-11-25 11:11:33 +0700

    Import sửa lại ClearChangeTracker() Inventory

--- CafeManager/ViewModels/AdminViewModel/InventoryViewModel.cs
+++ CafeManager/ViewModels/AdminViewModel/InventoryViewModel.cs
@@ -2,8 +2,10 @@
 using CafeManager.Core.Data;
 using CafeManager.Core.DTOs;
 using CafeManager.Core.Services;
+using CafeManager.WPF.MessageBox;
 using CafeManager.WPF.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections.ObjectModel;
 
@@ -15,6 +17,20 @@ namespace CafeManager.WPF.ViewModels.AdminViewModel
         private readonly MaterialSupplierServices _materialSupplierServices;
         private readonly ConsumedMaterialServices _consumedMaterialServices;
         private readonly IMapper _mapper;
+
+        [ObservableProperty]
+        private bool _isPopupOpen = false;
+
+        [ObservableProperty]
+        private bool _isAdding = false;
+
+        [ObservableProperty]
+        private bool _isUpdating = false;
+
+        private int _currentMaterialsupplierid = 0;
+
+        [ObservableProperty]
+        private ConsumedMaterialDTO _currentConsumedMaterial = new();
 
         [ObservableProperty]
         private ObservableCollection<ConsumedMaterialDTO> _listConsumedMaterialDTO = [];
@@ -41,6 +57,82 @@ namespace CafeManager.WPF.ViewModels.AdminViewModel
             ListInventoryDTO = [.. _mapper.Map<List<MaterialSupplierDTO>>(dbMaterialSupplier)];
 
             var list = _mapper.Map<List<Consumedmaterial>>(ListConsumedMaterialDTO);
+        }
+
+        public void ClearValueOfPopupBox()
+        {
+            IsAdding = false;
+            IsUpdating = false;
+            CurrentConsumedMaterial = new();
+        }
+
+        [RelayCommand]
+        private void OpenPopup_Add(MaterialSupplierDTO materialSupplierDTO)
+        {
+            _currentMaterialsupplierid = materialSupplierDTO.Materialsupplierid;
+            IsPopupOpen = true;
+            IsAdding = true;
+        }
+        [RelayCommand]
+        private void OpenPopup_Update(ConsumedMaterialDTO consumedMaterialDTO)
+        {
+            _currentMaterialsupplierid = consumedMaterialDTO.Materialsupplierid ?? 0;
+            IsPopupOpen = true;
+            IsAdding = true;
+        }
+
+        [RelayCommand]
+        private void ModifyStaffSalaryHistory()
+        {
+            if (IsAdding)
+            {
+                var existing = ListConsumedMaterialDTO.FirstOrDefault(x =>
+                    x.Effectivedate.Month == CurrentStaffSalary.Effectivedate.Month &&
+                    x.Effectivedate.Year == CurrentStaffSalary.Effectivedate.Year);
+
+                if (existing != null)
+                {
+                    MyMessageBox.ShowDialog("Ngày hiệu lực thêm mới bị trùng.");
+                }
+                else
+                {
+                    ModifyStaff.Staffsalaryhistories.Add(CurrentStaffSalary);
+                    _currentStaffSalaryIndex = ModifyStaff.Staffsalaryhistories.IndexOf(CurrentStaffSalary);
+                    MyMessageBox.Show("Thêm lịch sử lương thành công.");
+                }
+
+                IsAddingStaffSalaryHistory = false;
+            }
+
+            if (IsUpdatingStaffSalaryHistory)
+            {
+                bool ok = false;
+
+                for (int i = 0; i < ModifyStaff.Staffsalaryhistories.Count; i++)
+                {
+                    StaffsalaryhistoryDTO staffsalaryhistory = ModifyStaff.Staffsalaryhistories[i];
+                    if (i != _currentStaffSalaryIndex && CurrentStaffSalary.Effectivedate.Month == staffsalaryhistory.Effectivedate.Month && CurrentStaffSalary.Effectivedate.Year == staffsalaryhistory.Effectivedate.Year)
+                    {
+                        MyMessageBox.Show("Ngày hiệu lực thay đổi bị trùng");
+                        ok = true;
+                        break;
+                    }
+                }
+                if (!ok)
+                {
+                    ModifyStaff.Staffsalaryhistories[_currentStaffSalaryIndex].Effectivedate = CurrentStaffSalary.Effectivedate;
+                    ModifyStaff.Staffsalaryhistories[_currentStaffSalaryIndex].Salary = CurrentStaffSalary.Salary;
+                    MyMessageBox.Show("Sửa lịch sử lương thành công.");
+                }
+                IsUpdatingStaffSalaryHistory = false;
+            }
+
+            // Reset lại `CurrentStaffSalary` sau khi thêm/sửa
+            CurrentStaffSalary = new()
+            {
+                Effectivedate = DateOnly.FromDateTime(DateTime.Now)
+            };
+            IsPopupOpen = false;
         }
     }
 }
